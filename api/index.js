2f7c7be21e62f586f55d95e244af491c6ac0e4f6
hiramansha <160101518+hiramansha@users.noreply.github.com>
2026-09-21 08:46:19 +0000

    Fix Vercel API function

--- api/index.js
+++ api/index.js
@@ -5,9 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/api", (req, res) => {
-  res.status(200).json({
-    message: "Backend is working!"
-  });
+  res.json({ message: "Backend is working!" });
 });
 
 module.exports = app;
